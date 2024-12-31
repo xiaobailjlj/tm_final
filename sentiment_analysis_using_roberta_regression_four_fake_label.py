edf5070ea0a2a808389075d4fd2c2a52c31fb3e7
ViktorYiS <905205299@qq.com>
2024-12-31 22:17:31 +0100

    add fake label augmentation to regression.

--- sentiment_analysis_using_roberta_regression_four_fake_label.py
+++ sentiment_analysis_using_roberta_regression_four_fake_label.py
@@ -1,21 +1,20 @@
-# four classification
-# {'eval_loss': 1.1621623039245605, 'eval_accuracy': 0.46859903381642515, 'eval_runtime': 11.1691, 'eval_samples_per_second': 55.6, 'eval_steps_per_second': 3.492, 'epoch': 5.0}
+# four regression
+# {'eval_loss': 0.746597945690155, 'eval_model_preparation_time': 0.0013, 'eval_mse': 0.746597945690155, 'eval_mae': 0.7047160863876343, 'eval_r2': 0.11859804391860962, 'eval_regression_accuracy': 0.38164251207729466, 'eval_ordinal_accuracy': 0.38164251207729466, 'eval_runtime': 2.8967, 'eval_samples_per_second': 214.38, 'eval_steps_per_second': 13.463, 'epoch': 5.0}
 import os
 
+import numpy as np
 import pandas as pd
-from datasets import Dataset
 import torch
 from datasets import Dataset
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
-from torch.utils.data import DataLoader
-import numpy as np
 from evaluate import load
-from transformers import TrainingArguments
-from transformers import Trainer
-from evaluate import load
-from sklearn.metrics import mean_absolute_error, accuracy_score
+from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
+from transformers import Trainer
+from transformers import TrainingArguments
+
+from sentiment_analysis_using_roberta_classification_four_fake_label import train_on_fake_labels
 
 BASE_MODEL = "roberta-base"
 LEARNING_RATE = 2e-5
@@ -23,33 +22,37 @@ MAX_LENGTH = 256
 BATCH_SIZE = 16
 EPOCHS = 5
 
-# 4 labels, 0 1 2 3
-id2label = {k:k for k in range(4)}
-label2id = {k:k for k in range(4)}
-
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
-model = AutoModelForSequenceClassification.from_pretrained(BASE_MODEL, id2label=id2label, label2id=label2id)
-
-# tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
-# model = AutoModelForSequenceClassification.from_pretrained(
-#     BASE_MODEL,
-#     num_labels=1,
-#     problem_type="regression"  # Explicitly set as regression
-# )
-# # Modify the model's final layer to remove sigmoid activation
-# # This allows the model to predict unbounded values
-# model.classifier.activation = None
+model = AutoModelForSequenceClassification.from_pretrained(
+    BASE_MODEL,
+    num_labels=1,
+    problem_type="regression"  # Explicitly set as regression
+)
+# Modify the model's final layer to remove sigmoid activation
+# This allows the model to predict unbounded values
+model.classifier.activation = None
 
 
 
-device = 'cpu'
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 model.to(device)
 
+def preprocess_function(examples):
+    if "bias_score" in examples and examples["bias_score"] is not None:
+        label = min(float(examples["bias_score"]), 3.0)
+        label = max(label, 0.0)
+    else:
+        label = 0.0
+    results = tokenizer(examples["sentence_text"], truncation=True, padding="max_length", max_length=256)
+    results["label"] = float(label)
+    return results
+
 def load_data():
-    train = pd.read_csv('./news_bias_dataset/preprocessed_dataset.csv', delimiter=',')
-    print(train['bias_score'].unique())
-    print(train.describe())
-    new_df = train[['sentence_text', 'bias_score']]
+    train_raw_dataset = pd.read_csv('./news_bias_dataset/preprocessed_dataset.csv', delimiter=',')
+    print(train_raw_dataset['bias_score'].unique())
+    print(train_raw_dataset.describe())
+    new_df = train_raw_dataset[['sentence_text', 'bias_score']]
+
     train_size = 0.7
     valid_size = 0.5
     train_data = new_df.sample(frac=train_size, random_state=200)
@@ -71,30 +74,6 @@ def load_data():
     raw_test_ds = Dataset.from_pandas(test_data)
 
     return raw_train_ds, raw_valid_ds, raw_test_ds
-
-
-def preprocess_function(examples):
-    if "bias_score" in examples and examples["bias_score"] is not None:
-        label = min(int(examples["bias_score"]), 3)
-        label = max(label, 0)
-    else:
-        label = 0
-    results = tokenizer(examples["sentence_text"], truncation=True, padding="max_length", max_length=256)
-    results["label"] = int(label)
-    return results
-
-# def preprocess_function(examples):
-#     # Ensure labels are properly scaled between 0 and 1
-#     label = float(examples["bias_score"]) / 3.0  # This scales them to 0-1
-#
-#     result = tokenizer(
-#         examples["sentence_text"],
-#         truncation=True,
-#         padding="max_length",
-#         max_length=MAX_LENGTH
-#     )
-#     result["label"] = label
-#     return result
 
 
 def compute_metrics(eval_pred):
@@ -119,12 +98,12 @@ def compute_metrics_for_regression(eval_pred):
     r2 = r2_score(labels, logits)
 
     # Calculate accuracy with a tolerance
-    # Use 0.125 tolerance (slightly less than half the distance between classes)
-    tolerance = 0.125
+    # Use 0.5 tolerance (slightly less than half the distance between classes)
+    tolerance = 0.5
     accuracy = np.mean(np.abs(logits - labels) < tolerance)
 
-    pred_classes = np.round(logits * 3).clip(0, 3)
-    true_classes = np.round(labels * 3).clip(0, 3)
+    pred_classes = np.round(logits).clip(0, 3)
+    true_classes = np.round(labels).clip(0, 3)
     ordinal_accuracy = np.mean(pred_classes == true_classes)
 
     return {
@@ -135,24 +114,26 @@ def compute_metrics_for_regression(eval_pred):
         "ordinal_accuracy": ordinal_accuracy
     }
 
+class RegressionTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.pop("labels")
+        outputs = model(**inputs)
+        logits = outputs[0][:, 0]
+        loss = torch.nn.functional.mse_loss(logits, labels)
+        return (loss, outputs) if return_outputs else loss
 
-def train_on_fake_labels(trainer, ds, raw_train_ds, raw_test_ds, map_function=preprocess_function):
+
+def train_on_fake_labels_on_regression(trainer, ds, raw_train_ds, raw_test_ds, map_function=preprocess_function):
     new_data = pd.read_csv('./news_bias_dataset/augmented_dataset.csv', delimiter=',')
     new_ds = Dataset.from_pandas(new_data)
     new_ds = new_ds.map(map_function, remove_columns=["sentence_text"])
     predictions = trainer.predict(new_ds)
 
     # Extract true labels and predicted labels
-    logits = predictions.predictions
-    logits = logits - np.min(logits, axis=1, keepdims=True)  # Subtract the minimum value
-    logits = logits / np.sum(logits, axis=1, keepdims=True)  # Normalize by the sum
-    print(f"logits: {logits}")
-    predicted_labels = np.argmax(logits, axis=1)
-    confidences = np.max(logits, axis=1)
+    logits = np.round(predictions.predictions).clip(0, 3)
 
-    high_confidence_indices = np.where(confidences > 0.45)[0]
-    high_confidence_data = new_data.iloc[high_confidence_indices]
-    high_confidence_data['bias_score'] = predicted_labels[high_confidence_indices]
+    high_confidence_data = new_data
+    high_confidence_data['bias_score'] = logits
 
     # Split 30% of high_confidence_data into test set
     test_size = 0.3
@@ -172,6 +153,8 @@ def train_on_fake_labels(trainer, ds, raw_train_ds, raw_test_ds, map_function=pr
     ds["test"] = Dataset.from_pandas(pd.concat([raw_test_ds.to_pandas(), high_confidence_test_data]))
     ds_test = ds["test"].map(map_function, remove_columns=["sentence_text", "bias_score"])
     trainer.eval_dataset = ds_test
+    print("len(ds_test): ", len(ds_test))
+    print("len(eval_dataset): ", len(trainer.eval_dataset))
 
     print("Final evaluation on test set")
     print(trainer.evaluate())
@@ -188,7 +171,6 @@ if __name__ == '__main__':
     ds = {"train": raw_train_ds, "valid": raw_valid_ds, "test": raw_test_ds}
     for split in ds:
         ds[split] = ds[split].map(preprocess_function, remove_columns=["sentence_text", "bias_score"])
-        # ds[split] = ds[split].map(preprocess_function)
 
     metric = load("accuracy")
 
@@ -201,22 +183,23 @@ if __name__ == '__main__':
         num_train_epochs=EPOCHS,
         evaluation_strategy="epoch",
         save_strategy="epoch",
-        metric_for_best_model="accuracy",
+        metric_for_best_model="mse",
         load_best_model_at_end=True,
         weight_decay=0.01,
     )
 
-    trainer = Trainer(
+    trainer = RegressionTrainer(
         model=model,
         args=training_args,
         train_dataset=ds["train"],
         eval_dataset=ds["valid"],
-        compute_metrics=compute_metrics,
+        compute_metrics=compute_metrics_for_regression,
         tokenizer=tokenizer,  # Ensure the tokenizer is passed
         data_collator=DataCollatorWithPadding(tokenizer=tokenizer),  # Optional: Handle padding dynamically
     )
 
-    output_model_file = './models/sentiment_analysis_using_roberta_classification.bin'
+
+    output_model_file = './models/sentiment_analysis_using_roberta_regression.bin'
     output_vocab_file = './models/'
 
     if os.path.exists(output_model_file):
@@ -240,6 +223,10 @@ if __name__ == '__main__':
     # Perform evaluation
     predictions = trainer.predict(trainer.eval_dataset)
 
+    # Extract true labels and predicted labels
+    true_labels = predictions.label_ids  # Ground truth labels
+    predicted_labels = predictions.predictions.argmax(axis=1)  # Predicted labels (for classification tasks)
+
     for i in range(3):
-        trainer, ds = train_on_fake_labels(trainer, ds, raw_train_ds, raw_test_ds)
+        trainer, ds = train_on_fake_labels_on_regression(trainer, ds, raw_train_ds, raw_test_ds, map_function=preprocess_function)
 
